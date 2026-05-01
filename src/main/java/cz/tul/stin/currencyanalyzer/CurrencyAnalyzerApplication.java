48acba0fe69e5f96704bab78b3f878f3908bf971
Sergei Limonov <sergei.limonov@tul.cz>
2026-05-01 21:00:12 +0200

    Initial spring boot project

---
+++ src/main/java/cz/tul/stin/currencyanalyzer/CurrencyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.currencyanalyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurrencyAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/tul/stin/currencyanalyzer/CurrencyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.tul.stin.currencyanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
